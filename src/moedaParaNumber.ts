de94546755fcb0e4395f008aaa1f9f2e1f20019a
niusdev <vinidamascenadev@gmail.com>
2026-07-02 22:29:17 -0300

    refactor (moedaParaNumber)

--- src/moedaParaNumber.ts
+++ src/moedaParaNumber.ts
@@ -1,9 +1,5 @@
 //moeda = "452,00" | "-"
 export default function moedaParaNumber(moeda: string): number | null {
-    const numero = Number(moeda.replaceAll('.', '').replace(',', '.'));
-    if(isNaN(numero)){
-        return null;
-    } else {
-        return numero;
-    }
+  const numero = Number(moeda.replaceAll(".", "").replace(",", "."));
+  return isNaN(numero) ? null : numero;
 }
