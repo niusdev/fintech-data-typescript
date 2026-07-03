300075f8f29554f496c7dceaba2d00be4388a03d
niusdev <vinidamascenadev@gmail.com>
2026-07-02 21:50:37 -0300

    feat: adicionar a função de normalizar transação

---
+++ src/normalizarTransacao.ts
@@ -0,0 +1,45 @@
+declare global {
+  type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
+  type TransacaoStatus =
+    | "Recusada pela operadora de cartão"
+    | "Aguardando pagamento"
+    | "Paga"
+    | "Estornada";
+
+  interface TransacaoAPI {
+    Status: TransacaoStatus;
+    ID: number;
+    Data: string;
+    Nome: string;
+    ["Forma de Pagamento"]: TransacaoPagamento;
+    Email: string;
+    ["Valor (R$)"]: string;
+    ["Cliente Novo"]: number;
+  }
+
+  interface Trasacao {
+    nome: string;
+    id: number;
+    data: string;
+    status: TransacaoStatus;
+    email: string;
+    moeda: string;
+    valor: number | null;
+    pagamento: TransacaoPagamento;
+    novo: boolean;
+  }
+}
+
+export default async function normalizarTransacao(transacao: TransacaoAPI) {
+  return {
+    nome: transacao.Nome,
+    id: transacao.ID,
+    data: transacao.Data,
+    status: transacao.Status,
+    email: transacao.Email,
+    moeda: transacao["Valor (R$)"],
+    valor: 0,
+    pagamento: transacao["Forma de Pagamento"],
+    novo: Boolean(transacao["Cliente Novo"]),
+  };
+}
--- src/script.ts
+++ src/script.ts
@@ -1,29 +1,13 @@
 import fetchData from "./fetchData.js";
-type TransacaoPagamento = "Boleto" | "Cartão de Crédito";
-type TransacaoStatus =
-  | "Recusada pela operadora de cartão"
-  | "Aguardando pagamento"
-  | "Paga"
-  | "Estornada";
-
-interface TransacaoAPI {
-  Status: TransacaoStatus;
-  ID: number;
-  Data: string;
-  Nome: string;
-  ["Forma de Pagamento"]: TransacaoPagamento;
-  Email: string;
-  ["Valor (R$)"]: string;
-  ["Cliente Novo"]: number;
-}
+import normalizarTransacao from "./normalizarTransacao.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json",
   );
-  if (data) {
-    console.log(data);
-  }
+  if (!data) return;
+  const transacoes = data.map((t) => normalizarTransacao(t));
+  
 }
 
 handleData();
