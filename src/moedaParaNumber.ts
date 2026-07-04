c860f2a6c3f90499cde99b3e06672ead7396cc8f
niusdev <vinidamascenadev@gmail.com>
2026-07-03 22:04:10 -0300

    feat: adicionar função stringToDate

--- src/moedaParaNumber.ts
+++ src/moedaParaNumber.ts
@@ -1,4 +1,7 @@
-//moeda = "452,00" | "-"
+/**
+ * @param moeda : string. Exemplo: '1.250,00' 
+ * @returns number. Exemplo: '1250.00'
+ */
 export default function moedaParaNumber(moeda: string): number | null {
   const numero = Number(moeda.replaceAll(".", "").replace(",", "."));
   return isNaN(numero) ? null : numero;
--- src/script.ts
+++ src/script.ts
@@ -1,19 +1,15 @@
 import fetchData from "./fetchData.js";
 import moedaParaNumber from "./moedaParaNumber.js";
 import normalizarTransacao from "./normalizarTransacao.js";
+import stringToDate from "./stringToData.js";
 
 async function handleData() {
   const data = await fetchData<TransacaoAPI[]>(
     "https://api.origamid.dev/json/transacoes.json",
   );
   if (!data) return;
-  
+
   const transacoes = data.map((t) => normalizarTransacao(t));
-
-  transacoes.forEach((t) => {
-    moedaParaNumber(t.moeda);
-  });
-
   console.log(transacoes);
 }
 
